2aefe46ac55f1710a5d615e1e59ddb8855367aba
Rasulov Baxtzod <141494256+Baxtzodjon@users.noreply.github.com>
2023-08-13 10:32:44 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,35 @@
+let lorem = 'Lorem, ipsum dolor zit amet consectetur adipisicing elit. Voluptatibus doloremque, culpa voluptate exercitationem veritatis illum ipsum alias, repudiandae minus maiores animi iure eveniet placeat. Animi repellat perspiciatis natus ab! Molestias?'
+
+let b = ''
+
+console.log(lorem.slice(67,68).trim());
+
+let a = ''
+
+console.log(lorem.slice(22, 24).trim());
+
+let x = ''
+
+console.log(lorem.slice(100, 101));
+
+
+let t = ''
+
+console.log(lorem.slice(21, 23));
+
+
+let z = ''
+
+console.log(lorem.slice(18, 20).trim());
+
+let o = ''
+
+console.log(lorem.slice(1, 2));
+
+let d = ''
+
+console.log(lorem.slice(13, 14));
+
+
+let name = ''
+console.log(`b` + `a` + `x` + `t` + `z` + `o` + `d`);
